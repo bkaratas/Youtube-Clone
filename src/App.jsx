b8594b45903d39f054651875559a58ad44dbea57
Berat Karataş <96394976+bkaratas@users.noreply.github.com>
2025-01-12 18:49:30 +0300

    Update App.js
    Update App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home/home";
+import Home from "./Pages/Home/Home";
 import Video from "./Pages/Video/Video"
 
 const App = () => {
